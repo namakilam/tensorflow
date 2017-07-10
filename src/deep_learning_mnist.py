ae6018751b3235665afc9323e72e33f1f105c88f
Aman Malik <aman.malik@snapdeal.com>
2017-07-10 16:35:32 +0530

    Selu approximation

--- src/deep_learning_mnist.py
+++ src/deep_learning_mnist.py
@@ -17,25 +17,30 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+def selu(x):
+    alpha = 1.6732632423543772848170429916717
+    scale = 1.0507009873554804934193349852946
+    return scale*tf.where(x>=0.0, x, alpha*tf.exp(x)-alpha)
+
 W_l1 = init_weights([5, 5, 1, 32])
 B_l1 = init_bias([32])
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
 x_image = tf.reshape(x, [-1, 28, 28, 1])
-o_c1 = tf.nn.relu(conv2d(x_image, W_l1) + B_l1)
+o_c1 = selu(conv2d(x_image, W_l1) + B_l1)
 o_p1 = max_pool_2x2(o_c1)
 
 W_l2 = init_weights([5, 5, 32, 64])
 B_l2 = init_bias([64])
 
-o_c2 = tf.nn.relu(conv2d(o_p1, W_l2) + B_l2)
+o_c2 = selu(conv2d(o_p1, W_l2) + B_l2)
 o_p2 = max_pool_2x2(o_c2)
 
 W_l3 = init_weights([7 * 7 * 64, 1024])
 B_l3 = init_bias([1024])
 
 o_p2_flat = tf.reshape(o_p2, [-1, 7*7*64])
-o_l3 = tf.nn.relu(tf.matmul(o_p2_flat, W_l3) + B_l3)
+o_l3 = selu(tf.matmul(o_p2_flat, W_l3) + B_l3)
 
 W_l4 = init_weights([1024, 10])
 B_l4 = init_bias([10])
@@ -50,7 +55,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for i in range(10000):
+  for i in range(2000):
     batch = mnsit.train.next_batch(50)
     if i % 100 == 0:
       train_accuracy = accuracy.eval(feed_dict={
